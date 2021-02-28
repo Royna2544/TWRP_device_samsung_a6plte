8e0aa254b03806d39d64b3993ac15c556fed5aeb
roynatech2544 <69458352+roynatech2544@users.noreply.github.com>
2021-02-28 19:50:28 +0900

    Create omni_a6plte.mk

---
+++ omni_a6plte.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2019 The Android Open Source Project
+# Copyright (C) 2019 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+# Not available in android-10.0+ source
+# $(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit Telephony packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit language packages
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit 64bit support
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a6plte
+PRODUCT_NAME := omni_a6plte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A605K
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_RELEASE_NAME := Galaxy Jean
